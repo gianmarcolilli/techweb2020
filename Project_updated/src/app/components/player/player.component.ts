1c93c77d0d841bac4d5430e66d29d4b8ee8b5bb9
Riccardo <riccardo.marchini2@studio.unibo.it>
2020-10-29 19:46:09 +0100

    gestione storie / fascia d'età

--- Project_updated/src/app/components/player/player.component.ts
+++ Project_updated/src/app/components/player/player.component.ts
@@ -1,4 +1,6 @@
+import { TypeScriptEmitter } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { DummyApiService } from 'src/app/services/dummy-api.service';
 
 @Component({
   selector: 'app-player',
@@ -10,10 +12,21 @@ export class PlayerComponent implements OnInit {
   titolo: string = "Player";
   tipologiaGruppo = "";
   fasciaEta = "";
+  storie = [];
+  traccia = -1;
 
-  constructor() { }
+  getStories() {
+    this.apiDb.getStories().subscribe( (storietemp) => {
+      this.storie = storietemp
+    })
+  }
+
+  constructor(private apiDb: DummyApiService) { }
 
   ngOnInit(): void {
+    this.getStories();
   }
+
+
 
 }
