e5ac4ae584e3ceeef8950cd83477af189f03d41e
Riccardo <riccardo.marchini2@studio.unibo.it>
2020-10-29 19:17:05 +0100

    Aggiunti dati di mock

--- Project_updated/src/app/services/dummy-api.service.ts
+++ Project_updated/src/app/services/dummy-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Storia } from '../interfaces/storia';
 
 @Injectable({
@@ -14,13 +14,49 @@ export class DummyApiService {
 
   constructor(public http: HttpClient) { }
 
+  storie = [
+    {
+      id: 0,
+      nome: 'Evoluzione dell\'uomo',
+      didascalia: "Ci troviamo 4 milioni di anni fa e sappiamo che nella scala evolutiva dell’uomo c\'&egrave una figura che intercorre tra la scimmia e l’homo sapiens.",
+      urlBackground: 'https://image.shutterstock.com/image-photo/background-yellow-color-260nw-339630764.jpg',
+      startText: "Comincia",
+      steps: [
+        {
+          numeroStep: 0,
+          titleStep: 'Quale è la figura?',
+          action: 'clickToGo',
+          backImg: "string",
+          actionImg: "",
+          answers: [' australopiteco', 'uomo di neandertal']
+        },
+        {
+          numeroStep: 1,
+          titleStep: 'siamo nello step due ?',
+          action: '4answers',
+          backImg: "string",
+          actionImg: "",
+          answers: [
+            {
+              content: ' australopiteco',
+              correct: true
+            }, {
+              content: ' scimmia',
+              correct: false
+            }
 
+          ]
+        }
+      ]
+    }
+  ]
 
   // get
   getStories(): Observable<any> {
-    return this.http.get("https://sheet.best/api/sheets/8993a7e2-11f8-476b-8495-98e00fcfd75e")
+    return of(this.storie)
+    // return this.http.get("https://sheet.best/api/sheets/8993a7e2-11f8-476b-8495-98e00fcfd75e")
   }
- 
+
 
   getStoria(id:number): Observable<any>{
     return this.http.get("https://sheet.best/api/sheets/8993a7e2-11f8-476b-8495-98e00fcfd75e")
