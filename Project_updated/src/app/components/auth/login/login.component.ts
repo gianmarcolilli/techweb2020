72da4bbd595bb7f0bdaffb9d05d0e04fcec74414
Giovanni Ceglia <giovanni.ceglia2@studio.unibo.it>
2020-12-03 18:12:54 +0100

    tolto loop login errato

--- Project_updated/src/app/components/auth/login/login.component.ts
+++ Project_updated/src/app/components/auth/login/login.component.ts
@@ -1,22 +1,40 @@
+import { Subscription } from 'rxjs';
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 import { AuthService } from "../auth.service";
 
 @Component({
+  selector: 'form-field-error-example',
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
   isLoading = false;
-
+  private authStatusSub : Subscription;
   constructor(public authService: AuthService) {}
 
   onLogin(form: NgForm) {
     if (form.invalid) {
+
       return;
     }
     this.isLoading = true;
     this.authService.login(form.value.email, form.value.password);
   }
+  ngOnInit(): void {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus=>{
+        this.isLoading= false
+
+
+      }
+
+    )
+
+  }
 }
+
+
+
+
