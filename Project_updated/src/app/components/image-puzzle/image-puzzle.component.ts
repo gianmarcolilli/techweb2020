65fd20e5b005d99bede708207e4d1fad9cbf9ead
Riccardo <riccardo.marchini2@studio.unibo.it>
2021-01-25 14:59:46 +0100

    Prove puzzle

--- Project_updated/src/app/components/image-puzzle/image-puzzle.component.ts
+++ Project_updated/src/app/components/image-puzzle/image-puzzle.component.ts
@@ -1,16 +1,267 @@
-// import { isNullOrUndefined } from 'util';
-import {
-  Component,
-  Injectable,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
-import { timer } from 'rxjs';
-import { VisualizzaComponent } from '../visualizza/visualizza.component';
+// // import { isNullOrUndefined } from 'util';
+// import {
+//   Component,
+//   Injectable,
+//   Input,
+//   OnChanges,
+//   OnInit,
+//   SimpleChanges,
+// } from '@angular/core';
+// import { timer } from 'rxjs';
+// import { VisualizzaComponent } from '../visualizza/visualizza.component';
 
-import { HostListener } from '@angular/core';
+// import { HostListener } from '@angular/core';
+
+// @Component({
+//   moduleId: module.id,
+//   selector: 'app-image-puzzle',
+//   templateUrl: './image-puzzle.component.html',
+//   styleUrls: ['./image-puzzle.component.scss'],
+// })
+// export class ImagePuzzleComponent implements OnInit {
+//   @Input('puzzleUrl') imageUrl: string = '';
+//   @Input('difficulty') difficulty: string = '2';
+
+//   screenHeight: number;
+//   screenWidth: number;
+
+
+
+//   imageSize: number;
+//   gridsize: number = 2;
+//   boxSize: number = 100 / (this.gridsize - 1);
+//   index: number = 0;
+//   totalBoxes: number = this.gridsize * this.gridsize;
+//   Image: any[] = [];
+//   imageName: string = this.imageUrl
+//     .substr(this.imageUrl.lastIndexOf('/') + 1)
+//     .split('.')[0];
+//   steps: number = 0;
+//   ticks: string = '0:00';
+//   timer: any = timer(0, 1000);
+//   timeVar: any;
+//   gameComplete: Boolean = false;
+
+//   indexes: number[] = [];
+//   position: number[] = [];
+
+
+//   constructor(private visComp: VisualizzaComponent) {
+//     this.getScreenSize();
+//     console.log("imageSize:" + this.imageSize)
+//   }
+
+//   @HostListener('window:resize', ['$event'])
+//   getScreenSize(event?) {
+//     this.reset();
+//     this.imageSize = window.innerWidth * 0.8;
+//     this.screenHeight = window.innerHeight;
+//     console.log(this.imageSize, this.screenHeight);
+
+//     this.initializeGame();
+//     this.breakImageParts();
+//     this.reRandomize();
+//   }
+
+//   //Metodo di inizializzazione URL
+//   //Risorsa web
+//   //Risorsa codificata (esempio base64)
+//   //Risorsa locale
+//   initImageUrl() {
+//     if (this.imageUrl.startsWith('http')) {
+//       this.imageUrl = this.imageUrl;
+//     } else if (this.imageUrl.startsWith('data:image/')) {
+//       this.imageUrl = this.imageUrl;
+//     } else {
+//       this.imageUrl = './assets/images/' + this.imageUrl;
+//     }
+//   }
+
+//   ngOnInit() {
+//     this.initImageUrl();
+//     this.startGame();
+//   }
+
+//   //Metodo controllo sorting degli indici
+//   isSorted(indexes): Boolean {
+//     let i: number = 0;
+//     for (i = 0; i < indexes.length; i++) {
+//       if (indexes[i] !== i) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+//   //Metodo per riassegnare indici delle immagini in cui è stata scomposta l' immagine completa
+//   //Il metodo restituisce un array di indici
+//   randomize(imageParts: any[]): any[] {
+//     let i = 0,
+//       img: any[] = [],
+//       ran = 0;
+//     for (i = 0; i < imageParts.length; i++) {
+//       ran = Math.floor(Math.random() * imageParts.length);
+//       while (imageParts[ran] == null) {
+//         ran = Math.floor(Math.random() * imageParts.length);
+//       }
+//       img.push(imageParts[ran]);
+//       this.position.push(imageParts[ran].index);
+//       imageParts[ran] = null;
+//     }
+//     this.printIndexes(this.indexes);
+//     this.printIndexes(this.position);
+//     return img;
+//   }
+
+//   //Metodo di settaggio dati realitivi all' operazione di trascinamento e salvati in oggetto DataTransfer
+//   onDragStart(event: any): void {
+//     event.dataTransfer.setData('data', event.target.id);
+//   }
+
+//   //Metodo get dei dati di trascinamento utilizzato per il confronto con il documento origine
+//   //se tutte le posizioni combaciano col file origine gameComplete viene settato a true
+//   //ogni azione avvalora di 1 il numero di step impiegati
+//   //timeVar registra il tempo impiegato a completare il puzzle
+//   onDrop(event: any): void {
+//     let origin = event.dataTransfer.getData('data');
+//     let dest = event.target.id;
+
+//     let originEl = document.getElementById(origin);
+//     let destEl = document.getElementById(dest);
+
+//     let origincss = originEl.style.cssText;
+//     let destcss = event.target.style.cssText;
+
+//     destEl.style.cssText = origincss;
+//     originEl.style.cssText = destcss;
+//     originEl.id = dest;
+//     destEl.id = origin;
+
+//     for (let i = 0; i < this.position.length; i++) {
+//       if (this.position[i].toString() === originEl.id) {
+//         this.position[i] = Number(destEl.id);
+//       } else if (this.position[i].toString() === destEl.id) {
+//         this.position[i] = Number(originEl.id);
+//       }
+//     }
+
+//     this.printIndexes(this.position);
+//     this.steps++;
+//     this.gameComplete = this.isSorted(this.position);
+//     if (this.gameComplete) {
+//       if (this.timeVar) {
+//         this.timeVar.unsubscribe();
+//       }
+//     }
+//   }
+
+//   avanzamentoStep(idQ) {
+//     if (this.gameComplete) {
+//       //Metodo del visualizzaComponent
+//       this.visComp.gestisciAvanzamento(undefined);
+//     }
+//   }
+
+//   allowDrop(event): void {
+//     event.preventDefault();
+//     event.target.style.opacity = 1;
+//   }
+
+//   //Metodo di stampa degli indici
+//   printIndexes(sorts: number[]): void {
+//     let i: number = 0,
+//       ind: string = '';
+//     for (i = 0; i < sorts.length; i++) {
+//       ind += sorts[i].toString() + ' , ';
+//     }
+//   }
+
+//   //Metodo per scomporre nuovamente l' immagine completata in maniera casuale
+//   reRandomize(): void {
+//     this.gameComplete = false;
+//     this.Image = this.randomize(this.Image);
+//   }
+
+//   //Metodo di inizializzazione degli elementi per effettuare un game
+//   //-InizializeGame: settaggio della griglia per difficoltà selezionata
+//   //-breakImageParts: suddivisione dell' immagine in parti
+//   //-reRandomize scomposizione degli indici delle posizioni dei blocchi
+//   //-nuova sottoscrizione al timer timeVar
+//   startGame(): void {
+//     this.getScreenSize();
+
+//     if (this.timeVar) {
+//       this.timeVar.unsubscribe();
+//     }
+//     this.timeVar = this.timer.subscribe((t) => {
+//       this.settime(t);
+//     });
+//   }
+
+//   //Formattazione dell' ora 'mm:ss'
+//   settime(t: number): void {
+//     this.ticks =
+//       Math.floor(t / 60).toLocaleString('en-US', {
+//         minimumIntegerDigits: 2,
+//         useGrouping: false,
+//       }) +
+//       ':' +
+//       (t % 60).toLocaleString('en-US', {
+//         minimumIntegerDigits: 2,
+//         useGrouping: false,
+//       });
+//   }
+
+//   //Metodo di suddivisione dell' immagine in base al livello di difficoltà selezionato nel configura e calcolato nell' initializeGame()
+//   breakImageParts(): void {
+//     for (this.index = 0; this.index < this.totalBoxes; this.index++) {
+//       const x: string = this.boxSize * (this.index % this.gridsize) + '%';
+//       const y: string =
+//         this.boxSize * Math.floor(this.index / this.gridsize) + '%';
+//       let img: ImageBox = new ImageBox();
+//       img.x_pos = x;
+//       img.y_pos = y;
+//       img.index = this.index;
+//       this.indexes.push(this.index);
+//       this.Image.push(img);
+//     }
+
+//     this.boxSize = this.imageSize / this.gridsize;
+//   }
+
+//   //Metodo di inizializzazione game
+//   //Impostazione della gridlist in relazione al numero di boxes per difficoltà selezionata:
+//   //Facile: 4 boxes
+//   //Medio: 9 boxes
+//   //Difficile: 16 boxes
+//   initializeGame(): void {
+//     this.gridsize = Number(this.difficulty);
+//     this.boxSize = 100 / (this.gridsize - 1);
+//     this.index = 0;
+//     this.totalBoxes = this.gridsize * this.gridsize;
+//   }
+
+//   //Metodo reset
+//   reset(): void {
+//     this.Image = [];
+//     this.indexes = [];
+//     this.position = [];
+//   }
+// }
+
+// class ImageBox {
+//   x_pos: string;
+//   y_pos: string;
+//   index: number;
+// }
+
+
+
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { VisualizzaComponent } from '../visualizza/visualizza.component';
+import * as _ from 'lodash';
+import { timer } from 'rxjs';
 
 @Component({
   moduleId: module.id,
@@ -19,86 +270,103 @@ import { HostListener } from '@angular/core';
   styleUrls: ['./image-puzzle.component.scss'],
 })
 export class ImagePuzzleComponent implements OnInit {
-  @Input('puzzleUrl') imageUrl: string = '';
-  @Input('difficulty') difficulty: string = '2';
 
-  screenHeight: number;
-  screenWidth: number;
+  @Input('puzzleUrl') imageUrl: string;
+  @Input('difficulty') difficulty: number;
 
 
-
-  imageSize: number;
-  gridsize: number = 2;
-  boxSize: number = 100 / (this.gridsize - 1);
   index: number = 0;
-  totalBoxes: number = this.gridsize * this.gridsize;
-  Image: any[] = [];
-  imageName: string = this.imageUrl
-    .substr(this.imageUrl.lastIndexOf('/') + 1)
-    .split('.')[0];
-  steps: number = 0;
-  ticks: string = '0:00';
-  timer: any = timer(0, 1000);
-  timeVar: any;
-  gameComplete: Boolean = false;
-
   indexes: number[] = [];
+  totalBoxes: number;
+  boxSize: number;
+  Image: any[] = [];
   position: number[] = [];
+  imageSize: number;
+  timeVar: any;
+  timer: any = timer(0, 1000);
+  ticks: string = '0:00';
+  imageName: string;
 
+  steps: number = 0;
+  isCompleted: boolean = false;
 
   constructor(private visComp: VisualizzaComponent) {
     this.getScreenSize();
-    console.log("imageSize:" + this.imageSize)
   }
+
+
+  //Metodo chiamato ogni volta che sposto una tessera (drop appunto significa rilascio)
+  drop(event: CdkDragDrop<number[]>) {
+    this.steps++
+    moveItemInArray(this.Image, event.previousIndex, event.currentIndex);
+    this.isCompleted = this.verifyAccomplishment()
+  }
+
+  //Permette di accedere allo step successivo
+  avanzamentoStep(idQ) {
+    if (this.isCompleted) {
+      this.visComp.gestisciAvanzamento(undefined);
+    }
+  }
+
+  //A ogni spostamenta di una tessera verifica che tutte siano nella posizione corretta
+  //Una volta che l'array è stato ordinato restituisce complete=true, così permettendo l'avanzamento
+  verifyAccomplishment() {
+    let complete = true
+
+    for (let i = 0; i < this.Image.length; i++) {
+      if (this.Image[i].index != i) {
+        complete = false;
+        break;
+      }
+    }
+    return complete
+  }
+
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?) {
     this.reset();
     this.imageSize = window.innerWidth * 0.8;
-    this.screenHeight = window.innerHeight;
-    console.log(this.imageSize, this.screenHeight);
-
     this.initializeGame();
     this.breakImageParts();
     this.reRandomize();
   }
 
-  //Metodo di inizializzazione URL
-  //Risorsa web
-  //Risorsa codificata (esempio base64)
-  //Risorsa locale
-  initImageUrl() {
-    if (this.imageUrl.startsWith('http')) {
-      this.imageUrl = this.imageUrl;
-    } else if (this.imageUrl.startsWith('data:image/')) {
-      this.imageUrl = this.imageUrl;
-    } else {
-      this.imageUrl = './assets/images/' + this.imageUrl;
+  //Metodo reset
+  reset(): void {
+    this.Image = [];
+    this.indexes = [];
+    this.position = [];
+  }
+
+  initializeGame(): void {
+    this.boxSize = this.imageSize / this.difficulty
+    this.index = 0;
+    this.totalBoxes = this.difficulty * this.difficulty;
+  }
+
+
+  breakImageParts() {
+    for (this.index = 0; this.index < this.totalBoxes; this.index++) {
+      const x: string = this.boxSize * (this.index % this.difficulty) + '%';
+      const y: string = this.boxSize * Math.floor(this.index / this.difficulty) + '%';
+      let img: ImageBox = new ImageBox();
+      img.x_pos = x;
+      img.y_pos = y;
+      img.index = this.index;
+      this.indexes.push(this.index);
+      this.Image.push(img);
     }
   }
 
-  ngOnInit() {
-    this.initImageUrl();
-    this.startGame();
+  reRandomize(): void {
+    this.isCompleted = false;
+    this.Image = this.randomize(this.Image);
   }
 
-  //Metodo controllo sorting degli indici
-  isSorted(indexes): Boolean {
-    let i: number = 0;
-    for (i = 0; i < indexes.length; i++) {
-      if (indexes[i] !== i) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  //Metodo per riassegnare indici delle immagini in cui è stata scomposta l' immagine completa
-  //Il metodo restituisce un array di indici
   randomize(imageParts: any[]): any[] {
-    let i = 0,
-      img: any[] = [],
-      ran = 0;
+    let i = 0, img: any[] = [], ran = 0;
     for (i = 0; i < imageParts.length; i++) {
       ran = Math.floor(Math.random() * imageParts.length);
       while (imageParts[ran] == null) {
@@ -113,61 +381,6 @@ export class ImagePuzzleComponent implements OnInit {
     return img;
   }
 
-  //Metodo di settaggio dati realitivi all' operazione di trascinamento e salvati in oggetto DataTransfer
-  onDragStart(event: any): void {
-    event.dataTransfer.setData('data', event.target.id);
-  }
-
-  //Metodo get dei dati di trascinamento utilizzato per il confronto con il documento origine
-  //se tutte le posizioni combaciano col file origine gameComplete viene settato a true
-  //ogni azione avvalora di 1 il numero di step impiegati
-  //timeVar registra il tempo impiegato a completare il puzzle
-  onDrop(event: any): void {
-    let origin = event.dataTransfer.getData('data');
-    let dest = event.target.id;
-
-    let originEl = document.getElementById(origin);
-    let destEl = document.getElementById(dest);
-
-    let origincss = originEl.style.cssText;
-    let destcss = event.target.style.cssText;
-
-    destEl.style.cssText = origincss;
-    originEl.style.cssText = destcss;
-    originEl.id = dest;
-    destEl.id = origin;
-
-    for (let i = 0; i < this.position.length; i++) {
-      if (this.position[i].toString() === originEl.id) {
-        this.position[i] = Number(destEl.id);
-      } else if (this.position[i].toString() === destEl.id) {
-        this.position[i] = Number(originEl.id);
-      }
-    }
-
-    this.printIndexes(this.position);
-    this.steps++;
-    this.gameComplete = this.isSorted(this.position);
-    if (this.gameComplete) {
-      if (this.timeVar) {
-        this.timeVar.unsubscribe();
-      }
-    }
-  }
-
-  avanzamentoStep(idQ) {
-    if (this.gameComplete) {
-      //Metodo del visualizzaComponent
-      this.visComp.gestisciAvanzamento(undefined);
-    }
-  }
-
-  allowDrop(event): void {
-    event.preventDefault();
-    event.target.style.opacity = 1;
-  }
-
-  //Metodo di stampa degli indici
   printIndexes(sorts: number[]): void {
     let i: number = 0,
       ind: string = '';
@@ -176,20 +389,18 @@ export class ImagePuzzleComponent implements OnInit {
     }
   }
 
-  //Metodo per scomporre nuovamente l' immagine completata in maniera casuale
-  reRandomize(): void {
-    this.gameComplete = false;
-    this.Image = this.randomize(this.Image);
+  initImageUrl() {
+    if (this.imageUrl.startsWith('http')) {
+      this.imageUrl = this.imageUrl;
+    } else if (this.imageUrl.startsWith('data:image/')) {
+      this.imageUrl = this.imageUrl;
+    } else {
+      this.imageUrl = './assets/images/' + this.imageUrl;
+    }
   }
 
-  //Metodo di inizializzazione degli elementi per effettuare un game
-  //-InizializeGame: settaggio della griglia per difficoltà selezionata
-  //-breakImageParts: suddivisione dell' immagine in parti
-  //-reRandomize scomposizione degli indici delle posizioni dei blocchi
-  //-nuova sottoscrizione al timer timeVar
   startGame(): void {
     this.getScreenSize();
-
     if (this.timeVar) {
       this.timeVar.unsubscribe();
     }
@@ -198,55 +409,24 @@ export class ImagePuzzleComponent implements OnInit {
     });
   }
 
-  //Formattazione dell' ora 'mm:ss'
   settime(t: number): void {
-    this.ticks =
-      Math.floor(t / 60).toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      }) +
-      ':' +
-      (t % 60).toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      });
+    this.ticks = Math.floor(t / 60).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    }) + ':' + (t % 60).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
   }
 
-  //Metodo di suddivisione dell' immagine in base al livello di difficoltà selezionato nel configura e calcolato nell' initializeGame()
-  breakImageParts(): void {
-    for (this.index = 0; this.index < this.totalBoxes; this.index++) {
-      const x: string = this.boxSize * (this.index % this.gridsize) + '%';
-      const y: string =
-        this.boxSize * Math.floor(this.index / this.gridsize) + '%';
-      let img: ImageBox = new ImageBox();
-      img.x_pos = x;
-      img.y_pos = y;
-      img.index = this.index;
-      this.indexes.push(this.index);
-      this.Image.push(img);
-    }
-
-    this.boxSize = this.imageSize / this.gridsize;
+  ngOnInit(): void {
+    this.imageName = this.imageUrl
+    .substr(this.imageUrl.lastIndexOf('/') + 1)
+    .split('.')[0];
+    this.initImageUrl();
+    this.startGame();
   }
 
-  //Metodo di inizializzazione game
-  //Impostazione della gridlist in relazione al numero di boxes per difficoltà selezionata:
-  //Facile: 4 boxes
-  //Medio: 9 boxes
-  //Difficile: 16 boxes
-  initializeGame(): void {
-    this.gridsize = Number(this.difficulty);
-    this.boxSize = 100 / (this.gridsize - 1);
-    this.index = 0;
-    this.totalBoxes = this.gridsize * this.gridsize;
-  }
-
-  //Metodo reset
-  reset(): void {
-    this.Image = [];
-    this.indexes = [];
-    this.position = [];
-  }
 }
 
 class ImageBox {
